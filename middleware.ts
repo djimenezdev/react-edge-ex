6a012c8ddf4f8a949228963a0db1f3f4a3239e25
Daniel Jimenez <daniel@djimenezdev.com>
2024-08-08 18:34:05 -0700

    test if env is being loaded properly

--- middleware.ts
+++ middleware.ts
@@ -7,12 +7,11 @@ export const config = {
 export default function middleware(request: Request) {
   const url = new URL(request.url);
   const privateVar = process.env.VITE_API_URL;
-  console.log(privateVar);
   if (url.pathname.startsWith("/nfts")) {
     return rewrite(new URL("/url", request.url));
   }
   if (url.pathname.startsWith("/about")) {
-    return new Response("Hello, from about");
+    return new Response(`Hello, from about. URL to api is ${privateVar}`);
   }
   return next();
 }
