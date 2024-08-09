1c5c7c752c61c76f4f9ea01826a4b42e88e82e15
Daniel Jimenez <daniel@djimenezdev.com>
2024-08-08 19:07:06 -0700

    Avoids limitations but checks for static files and just returns

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { rewrite, next } from "@vercel/edge";
+import { rewrite } from "@vercel/edge";
 
 export const config = {
   runtime: "edge",
@@ -10,11 +10,19 @@ export const config = {
 export default function middleware(request: Request) {
   const url = new URL(request.url);
   const privateVar = process.env.VITE_API_URL;
+
+  if (
+    url.pathname.match(
+      /\.(css|js|jsx|ts|tsx|jpg|jpeg|png|gif|ico|svg|woff|woff2|ttf|eot)$/
+    )
+  ) {
+    return;
+  }
+
   if (url.pathname.startsWith("/nfts")) {
     return rewrite(new URL("/url", request.url));
   }
   if (url.pathname.startsWith("/about")) {
     return new Response(`Hello, from about. URL to api is ${privateVar}`);
   }
-  return next();
 }
