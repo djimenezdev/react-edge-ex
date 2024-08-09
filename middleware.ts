4d733f891333296d6c3799ce5713c905b2b6a111
Daniel Jimenez <daniel@djimenezdev.com>
2024-08-08 19:04:35 -0700

    updated matcher

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { rewrite, next } from "@vercel/edge";
 export const config = {
   runtime: "edge",
   matcher: [
-    "/((?!\\.(css|js|jsx|ts|tsx|jpg|jpeg|png|gif|ico|svg|woff|woff2|ttf|eot)).*)",
+    "/:path*/(?!.*\\.(css|js|jsx|ts|tsx|jpg|jpeg|png|gif|ico|svg|woff|woff2|ttf|eot)$).*",
   ],
 };
 
