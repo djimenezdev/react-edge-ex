64918ad20d45f8796f37f15bdde2790c7507e049
Daniel Jimenez <daniel@djimenezdev.com>
2024-08-08 19:16:37 -0700

    test updated matcher

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,14 @@ import { rewrite } from "@vercel/edge";
 
 export const config = {
   runtime: "edge",
-  matcher: ["/((?!assets/|vite.svg).*)"],
+  matcher: [
+    "/((?!assets/|vite.svg|\\.(css|js|jsx|ts|tsx|jpg|jpeg|png|gif|ico|svg|woff|woff2|ttf|eot)).*)",
+  ],
 };
 
 export default function middleware(request: Request) {
   const url = new URL(request.url);
   const privateVar = process.env.VITE_API_URL;
-
-  if (
-    url.pathname.match(
-      /\.(css|js|jsx|ts|tsx|jpg|jpeg|png|gif|ico|svg|woff|woff2|ttf|eot)$/
-    )
-  ) {
-    return;
-  }
 
   if (url.pathname.startsWith("/nfts")) {
     return rewrite(new URL("/url", request.url));
